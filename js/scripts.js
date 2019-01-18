b3ac0f78526ebe3ce2456bfabe353e267dd43812
Jack <jackpaulcollins@gmail.com>
2019-01-18 10:31:19 -0800

    Begins to build js functionality. Adds function to show hidden row when submit button clicked

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+//front-end logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//back-end logic
+
+$(document).ready(function() {
+  $("#mainForm").submit(function(event) {
+  event.preventDefault();
+  $("#recommendation").show();
+  })
+})
