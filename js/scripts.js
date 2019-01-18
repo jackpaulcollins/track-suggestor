7c513860ee1045b45c07bb33b015bfd4990f675a
Jack <jackpaulcollins@gmail.com>
2019-01-18 10:48:24 -0800

    adds functionality to submit button - gives custom greeting to each card depending on user input for name

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //front-end logic
+//function chooseTrack(a,b,c,)
+//  return
 
+//  var javaScore =
+//  var rubyScore =
 
 
 
@@ -20,6 +24,12 @@
 $(document).ready(function() {
   $("#mainForm").submit(function(event) {
   event.preventDefault();
+  var question1Input = parseInt($("#question1").val());
+  var question2Input = parseInt($("#question2").val());
+  var question3Input = parseInt($("#question3").val());
+  var userName = $("input#userName").val();
+  console.log(userName)
+  $(".userNameAnswer").text(userName);
   $("#recommendation").show();
   })
 })
